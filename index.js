adf3d2e143c52a4fd2ea6333af4446bd86f21544
Gabriel Rodrigues <52265978+Apenasgabs@users.noreply.github.com>
2021-10-21 16:32:32 -0300

    inputs mapeados

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const puppeteer = require("puppeteer");
 const url = `https://forms.office.com/r/tBt7k8vTnD`
+const inputClick = [
+    ".office-form-question-element > div > div:nth-child(2) > div > label > span",
+    "div.office-form-question-body > div:nth-child(3) > div > div.office-form-question-element > div > div:nth-child(2) > div > label > span > span",
+    ".office-form-question-body > div:nth-child(4) > div > div.office-form-question-element > div > div:nth-child(2) > div > label > span > span",
+];
+const sendBtn = [
+    "div.office-form-body > div.office-form-navigation-container > div.office-form-button-container > button",
+];
 const robo = async(site) => {
     const browser = await puppeteer.launch({
         headless: false,
-        userDataDir: "./user_data"
+        userDataDir: "./user_data",
     });
     const page = await browser.newPage();
     await page.goto(site);
-    // await page.screenshot({ path: "example.png" });
-
-    // await browser.close();
+    const clicker = async(item) => {
+        await page.waitForSelector(item);
+        await page.click(item);
+    };
+    clicker(inputClick[0]);
 };
-robo(url);
+try {
+    robo(url);
+} catch (error) {
+    console.log(error);
+}
