2739254ce0af53a061e68af4905ea1b162d79683
ApenasGabs <Gabers357@gmail.com>
2021-10-21 23:16:04 -0300

    ✨projeto finalizado ✨

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
+require("dotenv").config();
 const puppeteer = require("puppeteer");
-const url = `https://forms.office.com/r/tBt7k8vTnD`
+
 const inputClick = [
-    ".office-form-question-element > div > div:nth-child(2) > div > label > span",
+    "div.office-form-question-element > div > div:nth-child(2) > div > label > span",
     "div.office-form-question-body > div:nth-child(3) > div > div.office-form-question-element > div > div:nth-child(2) > div > label > span > span",
-    ".office-form-question-body > div:nth-child(4) > div > div.office-form-question-element > div > div:nth-child(2) > div > label > span > span",
+    "div.office-form-question-body > div:nth-child(4) > div > div.office-form-question-element > div > div:nth-child(2) > div > label > span > span",
+    "div.office-form-body > div.office-form-navigation-container > div.office-form-button-container > button"
 ];
-const sendBtn = [
-    "div.office-form-body > div.office-form-navigation-container > div.office-form-button-container > button",
-];
-const robo = async(site) => {
+const link = process.env.FORM_URL
+const robo = async(url) => {
     const browser = await puppeteer.launch({
         headless: false,
         userDataDir: "./user_data",
     });
     const page = await browser.newPage();
-    await page.goto(site);
+    await page.goto(url);
     const clicker = async(item) => {
         await page.waitForSelector(item);
         await page.click(item);
     };
-    clicker(inputClick[0]);
+    inputClick.map(clicker);
 };
 try {
-    robo(url);
+    robo(link);
 } catch (error) {
     console.log(error);
 }
